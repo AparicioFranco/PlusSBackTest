d88da012cdb22e1e7ecd1f328ee197cd2ab09722
aparusho <franco.aparicio@ing.austral.edu.ar>
2022-05-01 16:45:17 -0300

    Changes

--- PlusSBackTest/settings.py
+++ PlusSBackTest/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^rgb1(+hlt%dda#hmgg9@yw7#b9pfq-r1oj(b&j^%$l^yu!g4_'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
