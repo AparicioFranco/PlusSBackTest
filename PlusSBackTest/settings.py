4e016b119a6314a78be61bf85954b1c77b926a9d
aparusho <franco.aparicio@ing.austral.edu.ar>
2022-05-01 17:00:56 -0300

    FinalCommit

--- PlusSBackTest/settings.py
+++ PlusSBackTest/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-^rgb1(+hlt%dda#hmgg9@yw7#b9pfq-r1oj(b&j^%$l^yu!g4_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'inquiry',
+    'inquiry'
 ]
 
 MIDDLEWARE = [
